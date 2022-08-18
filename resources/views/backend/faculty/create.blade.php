782c5c190c6cabade73cce71dfd2dc4338ecef5b
thuyntk <nguyenthikimthuy019@gmail.com>
2022-08-18 17:30:16 +0700

    faculty

--- resources/views/backend/faculty/create.blade.php
+++ resources/views/backend/faculty/create.blade.php
@@ -5,7 +5,7 @@
 {!! Form::open(['method' => 'POST', 'route' => ['faculties.store']])!!}
 <div class="form-group">
     {!! Form::label('name', 'Faculty Name') !!}
-    {!! Form::text('name', ['class' => 'form-control']) !!}
+    {!! Form::text('name','', ['class' => 'form-control']) !!}
 </div>
 <div>
     {!! Form::submit('Create', ['class' => 'btn btn-primary']) !!}
